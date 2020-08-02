853019c06c4eb12259a0e8ce43bed2ef73bbe6e7
Celmer John Madayag <cjmadayag24@gmail.com>
2020-08-03 03:43:10 +0800

    Updates

---
+++ precache-manifest.7cd1ed44f3fc3b1f2ff203d6febbd011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f7f9e4ef5819cb31a87ef726335d0e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "8d23a83617f579b75602",
+    "url": "/todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "adf6f40ea3279de5c628",
+    "url": "/todo/static/css/main.799492e1.chunk.css"
+  },
+  {
+    "revision": "8d23a83617f579b75602",
+    "url": "/todo/static/js/2.3315d12c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo/static/js/2.3315d12c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf6f40ea3279de5c628",
+    "url": "/todo/static/js/main.0fd1db4e.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
